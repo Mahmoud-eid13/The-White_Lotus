676e91da28262dd21cc37a1f0fbd32483a87e9a5
mahmoud eid <eid.mahmoud1312@gmail.com>
2025-02-13 20:51:33 +0200

    adding supabase configrations

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://shzanwqffsfmzwxspvje.supabase.co'
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNoemFud3FmZnNmbXp3eHNwdmplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk0NjMyNzcsImV4cCI6MjA1NTAzOTI3N30._YamHO-tIgg94DqdcHbAYCharZATJEFAZ0I8NPlsneY"
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase; 
